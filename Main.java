d01100aa10af035dd49ca926cddea05ba4afc58f
JGong7 <97704595+JGong7@users.noreply.github.com>
2024-05-06 17:19:50 -0700

    set up db on AWS and connect with JDBC server

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Connection URL
+        String url = "jdbc:postgresql://cse132b.cxa6600i8ci8.us-east-2.rds.amazonaws.com:5432/postgres";
+        String user = "postgres";
+        String password = "James2085";
+        String port = "5432";
+
+        try {
+            // Load the PostgreSQL JDBC driver
+            Class.forName("org.postgresql.Driver");
+
+            // Establish the connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            String sql = "CREATE TABLE Faculty (" +
+             "ID INT PRIMARY KEY     NOT NULL," +
+             "NAME           TEXT    NOT NULL," +
+             "AGE            INT     NOT NULL," +
+             "ADDRESS        CHAR(50)," +
+             "SALARY         REAL )";
+            stmt.executeUpdate(sql);
+
+            // After creating the table
+            String checkTableExists = "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'faculty')";
+            ResultSet rs = stmt.executeQuery(checkTableExists);
+            if (rs.next()) {
+                boolean exists = rs.getBoolean(1);
+                if (exists) {
+                    System.out.println("Table created successfully");
+                } else {
+                    System.out.println("Table creation failed");
+                }
+            }
+        
+            // If connection is successfully established, print a message
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+            // Close the connection
+            conn.close();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path.");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println("Connection Failed! Check output console.");
+            ex.printStackTrace();
+        }
+    }
+}
